a32c496e85b7aed73466dfdb11a30d5dcb2ed08b
DevM <dev.mukeshcgs@gmail.com>
2025-01-25 13:24:50 +0530

    token updated

--- index.js
+++ index.js
@@ -6,17 +6,34 @@ const { twitterClient } = require("./twitterClient.js")
 const CronJob = require("cron").CronJob;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+    console.log(`Listening on port ${port}`)
 })
 
-const tweet = async () => {
-  try {
-    await twitterClient.v2.tweet("Hello world!");
-  } catch (e) {
-    console.log(e)
-  }
-}
+// const tweet = async () => {
+//   try {
+//     await twitterClient.v2.tweet("Hello world!");
+//   } catch (e) {
+//     console.log(e)
+//   }
+// }
+// const cronTweet = new CronJob("30 * * * * *", async () => {
+//   tweet();
+// });
 
-const cronTweet = new CronJob("30 * * * * *", async () => {
-  tweet();
-});
+const search = async () => {
+    try {
+        const response = await twitterClient.v2.search("nodejs", { max_results: 10 });
+        console.log(response)
+    } catch (e) {
+        console.log(e)
+    }
+}
+const postTweet = async () => {
+    try {
+        await twitterClient.v2.tweet("Hello world!");
+    } catch (e) {
+        console.log(e)
+    }
+}
+postTweet();
+// search();
